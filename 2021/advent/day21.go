a98ecbee665a96cdc6fc3f1f39956d5e86e5094a
Desmond Daignault <dez@ziprecruiter.com>
2021-12-21 22:46:14 -0500

    day21 complete

--- 2021/advent/day21.go
+++ 2021/advent/day21.go
@@ -5,20 +5,15 @@ import (
 	"strings"
 )
 
+var (
+	p1_start int
+	p2_start int
+	seen     = map[string][2]int64{}
+)
+
 type Player struct {
-	name  string
 	pos   int
 	score int
-}
-
-type deterministic_die int
-
-func (d *deterministic_die) next() int {
-	*d++
-	if *d > 100 {
-		*d %= 100
-	}
-	return int(*d)
 }
 
 func (p *Player) move(steps int) {
@@ -30,53 +25,127 @@ func (p *Player) move(steps int) {
 		}
 	}
 	p.pos = new_pos
-	p.score += p.pos
-	fmt.Printf("score is now: %d\n", p.score)
+}
+
+type deterministic_die int
+
+func (d *deterministic_die) next() int {
+	sum := 0
+	for i := 0; i < 3; i++ {
+		*d++
+		if *d == 101 {
+			*d = 1
+		}
+		sum += int(*d)
+	}
+	return sum
+}
+
+type Game struct {
+	players    [2]Player
+	rolls_left int
+	turn       int // which player's turn it is
+}
+
+func start_game() (g Game) {
+	g.players[0] = Player{
+		pos:   p1_start,
+		score: 0,
+	}
+	g.players[1] = Player{
+		pos:   p2_start,
+		score: 0,
+	}
+	g.turn = 0
+	g.rolls_left = 3
+
+	return g
 }
 
 func day21(puzzle_data []string) {
-	/*
-		for _, dataVal := range puzzle_data {
-		}
-	*/
-	p1 := Player{
-		name:  "one",
-		score: 0,
-	}
-	p2 := Player{
-		name:  "two",
-		score: 0,
-	}
-
+	// parse input data
 	t := strings.Fields(puzzle_data[0])
-	p1.pos = makeInt(t[len(t)-1])
+	p1_start = makeInt(t[len(t)-1])
 	t = strings.Fields(puzzle_data[1])
-	p2.pos = makeInt(t[len(t)-1])
+	p2_start = makeInt(t[len(t)-1])
 
-	var d deterministic_die
+	game := start_game()
+
+	var shared_die deterministic_die
+	player1 := Player{
+		pos:   p1_start,
+		score: 0,
+	}
+	player2 := Player{
+		pos:   p2_start,
+		score: 0,
+	}
 
 	rolls := 0
-	for p1.score < 1000 && p2.score < 1000 {
-		for _, player := range []*Player{&p1, &p2} {
-			move := 0
-			fmt.Printf("Player %s rolled: ", player.name)
-			for i := 0; i < 3; i++ {
-				roll := d.next()
-				move += roll
-				rolls++
-				fmt.Printf("%d ", roll)
-			}
+	for player1.score < 1000 && player2.score < 1000 {
+		for _, player := range []*Player{&player1, &player2} {
+			move := shared_die.next()
+			rolls += 3
 			player.move(move)
+			player.score += player.pos
 			if player.score >= 1000 {
 				break
 			}
 		}
 	}
 
-	losing_score := p1.score
-	if p2.score < losing_score {
-		losing_score = p2.score
+	losing_score := player1.score
+	if player2.score < losing_score {
+		losing_score = player2.score
 	}
 
 	fmt.Printf("Day 21 (part 1): Losing score %d mul by rolls %d is %d\n", losing_score, rolls, losing_score*rolls)
+
+	game = start_game()
+
+	p1_wins, p2_wins := play(game)
+	num_universes := p1_wins
+	if p2_wins > p1_wins {
+		num_universes = p2_wins
+	}
+	fmt.Printf("Day 21 (part 2): Num universes for the winner in more universes is %d\n", num_universes)
+}
+
+func play(g Game) (p1_win, p2_win int64) {
+	key := fmt.Sprint(g)
+	if res, ok := seen[key]; ok {
+		return res[0], res[1]
+	}
+
+	if g.rolls_left == 0 {
+		g.players[g.turn].score += g.players[g.turn].pos
+		if g.players[g.turn].score > 20 {
+			if g.turn == 0 {
+				return 1, 0
+			} else {
+				return 0, 1
+			}
+		}
+		if g.turn == 0 {
+			g.turn = 1
+		} else {
+			g.turn = 0
+		}
+		g.rolls_left = 3
+	}
+	for roll := 1; roll <= 3; roll++ {
+		g_copy := Game{
+			players:    [2]Player{g.players[0], g.players[1]},
+			rolls_left: g.rolls_left - 1,
+			turn:       g.turn,
+		}
+		g_copy.players[g.turn].move(roll)
+		one, two := play(g_copy)
+		p1_win += one
+		p2_win += two
+	}
+
+	seen[key] = [2]int64{p1_win, p2_win}
+
+	return p1_win, p2_win
 }
